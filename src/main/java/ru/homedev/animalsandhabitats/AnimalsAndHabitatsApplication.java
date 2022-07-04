3d6e8ef87e90497a78834c3de1c4ec686d000167
Stepygin Sergey <stepygin.sergey@gmail.com>
2022-07-04 15:09:49 +0300

    Base Commit

---
+++ src/main/java/ru/homedev/animalsandhabitats/AnimalsAndHabitatsApplication.java
@@ -0,0 +1,13 @@
+package ru.homedev.animalsandhabitats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsAndHabitatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsAndHabitatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/homedev/animalsandhabitats/AnimalsAndHabitatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homedev.animalsandhabitats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsAndHabitatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
